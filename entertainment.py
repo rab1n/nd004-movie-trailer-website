e2f7b92e449341425a867cb01eeed1fd5f546d1b
Rabin Joshi <rabin.joshi@nrccua.org>
2017-02-26 16:06:43 -0600

    adds storyline

--- entertainment.py
+++ entertainment.py
@@ -4,15 +4,19 @@ import fresh_tomatoes
 # instantiates 3 movies
 la_la_land = media.Movie("La La Land",
                          "https://upload.wikimedia.org/wikipedia/en/a/ab/La_La_Land_(film).png",
-                         "https://youtu.be/0pdqf4P9MB8")
+                         "https://youtu.be/0pdqf4P9MB8",
+                         "The story of an aspiring actress and a jazz musician pursuing their dreams.")
 
 captain_america = media.Movie("Captain America: Civil War",
                               "https://upload.wikimedia.org/wikipedia/en/5/53/Captain_America_Civil_War_poster.jpg",
-                              "https://youtu.be/dKrVegVI0Us")
+                              "https://youtu.be/dKrVegVI0Us",
+                              "An American superhero film based on the Marvel Comics character Captain America."
+                              )
 
 star_wars = media.Movie("Rogue One: A Star Wars Story",
                         "https://upload.wikimedia.org/wikipedia/en/d/d4/Rogue_One%2C_A_Star_Wars_Story_poster.png",
-                        "https://youtu.be/frdj1zb9sMY")
+                        "https://youtu.be/frdj1zb9sMY",
+                        "Rogue One follows a group of rebels on a mission to steal the plans for the Death Star.")
 
 # adds the movies to an array
 movies = [la_la_land, captain_america, star_wars]
--- media.py
+++ media.py
@@ -2,7 +2,8 @@ import webbrowser
 
 
 class Movie():
-    def __init__(self, title, poster_image_url, trailer_youtube_url):
+    def __init__(self, title, poster_image_url, trailer_youtube_url, storyline):
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
+        self.storyline = storyline
