033689c050095faf9e91ac85349370eb4cae09d4
Rabin Joshi <rabin.joshi@nrccua.org>
2017-02-26 16:18:47 -0600

    adds more comments

--- entertainment.py
+++ entertainment.py
@@ -1,7 +1,8 @@
 import media
 import fresh_tomatoes
 
-# instantiates 3 movies
+# Creating multiple instances of that Python Class to represent my favorite movies.
+
 la_la_land = media.Movie("La La Land",
                          "https://upload.wikimedia.org/wikipedia/en/a/ab/La_La_Land_(film).png",
                          "https://youtu.be/0pdqf4P9MB8",
@@ -18,8 +19,8 @@ star_wars = media.Movie("Rogue One: A Star Wars Story",
                         "https://youtu.be/frdj1zb9sMY",
                         "Rogue One follows a group of rebels on a mission to steal the plans for the Death Star.")
 
-# adds the movies to an array
+#  Grouping all the instances together in a list.
 movies = [la_la_land, captain_america, star_wars]
 
-# calls function on fresh_tomatoes.py to render the movies
+# This function call opens a page of movies generated from the input, an array of movie instances.
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -2,6 +2,9 @@ import webbrowser
 
 
 class Movie():
+    """This class serves as a data structure to store one's favorite movies, including movie title, poster URL,
+     YouTube link to the movie trailer and it's storyline."""
+
     def __init__(self, title, poster_image_url, trailer_youtube_url, storyline):
         self.title = title
         self.poster_image_url = poster_image_url
